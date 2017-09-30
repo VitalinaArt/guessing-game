af0d2a8e878477488692bd94f610b05f3612a5ee
Vitalina Artemchuk <artemchukvitalina@gmail.com>
2017-09-30 21:33:00 +0300

    Task1

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+ this.max=max;
+ this.min=min;
     }
 
     guess() {
-
+return Math.ceil((this.max+this.min)/2);
     }
 
     lower() {
-
+      this.max=Math.ceil((this.max+this.min)/2);
     }
 
     greater() {
-
+      this.min=Math.ceil((this.max+this.min)/2);
     }
 }
 
